031451fe4791015e124edd81393ef7acdc3051e5
Andrew Seaman <aseaman@23andme.com>
2018-11-15 10:10:11 -0800

    Deploy based on env and branch

--- scripts/deploy_api.sh
+++ scripts/deploy_api.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
-    make deploy
+    DEPLOY_ENV="$stage" BRANCH="$TRAVIS_BRANCH" make deploy
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
+    DEPLOY_ENV="$live" BRANCH="$TRAVIS_BRANCH" make deploy
+else
+    DEPLOY_ENV="$test" BRANCH="$TRAVIS_BRANCH" make deploy
 fi
