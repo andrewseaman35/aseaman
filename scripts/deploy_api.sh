d74485dc5fecfcdb92d580af5644e8dfdd79ecd3
Andrew Seaman <aseaman@23andme.com>
2018-11-20 08:03:46 -0800

    Remove source for venv

--- scripts/deploy_api.sh
+++ scripts/deploy_api.sh
@@ -2,17 +2,28 @@
 
 export \
     BRANCH="$TRAVIS_BRANCH" \
-    NONCE=$(date +%s) \
+    NONCE=$(date +%s)
+
+export \
     STACKNAME=stack-$(DEPLOY_ENV)-$(NONCE) \
     LAMBDA_FUNCTION_NAME=lambda-api-$(DEPLOY_ENV)-$(NONCE)
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     export DEPLOY_ENV="stage"
-    make deploy
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
     export DEPLOY_ENV="live"
-    make deploy
 else
     export DEPLOY_ENV="test"
+fi
+
+export \
+    STACKNAME=stack-$(DEPLOY_ENV)-$(NONCE) \
+    LAMBDA_FUNCTION_NAME=lambda-api-$(DEPLOY_ENV)-$(NONCE)
+
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    make deploy
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
+    make deploy
+else
     make deploy_test
 fi
