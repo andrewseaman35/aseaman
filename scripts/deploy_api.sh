187875e291f76fea10aa5ca2b1e922baaa151da6
Andrew Seaman <aseaman@23andme.com>
2018-11-20 12:23:49 -0800

    fix exports in deploy_api

--- scripts/deploy_api.sh
+++ scripts/deploy_api.sh
@@ -4,10 +4,6 @@ export \
     BRANCH="$TRAVIS_BRANCH" \
     NONCE=$(date +%s)
 
-export \
-    STACKNAME=stack-$(DEPLOY_ENV)-$(NONCE) \
-    LAMBDA_FUNCTION_NAME=lambda-api-$(DEPLOY_ENV)-$(NONCE)
-
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     export DEPLOY_ENV="stage"
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
@@ -17,8 +13,8 @@ else
 fi
 
 export \
-    STACKNAME=stack-$(DEPLOY_ENV)-$(NONCE) \
-    LAMBDA_FUNCTION_NAME=lambda-api-$(DEPLOY_ENV)-$(NONCE)
+    STACKNAME=stack-$DEPLOY_ENV-$NONCE \
+    LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$NONCE
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     make deploy
