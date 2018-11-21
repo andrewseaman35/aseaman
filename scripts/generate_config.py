385bc6854c66317145490d8d03cc2b50ae4d9d21
Andrew Seaman <andrewseaman35@gmail.com>
2018-11-20 17:08:44 -0800

    Enable access to stack params in JS config (#16)
    * Add cloudformation outputs to js config
    
    * enable key override since stack outputs must be alphanumeric
    
    * remove boto3 profile specification

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -1,27 +1,61 @@
+import boto3
 import json
 import os
 
 VALID_ENVS = {'test', 'stage', 'live'}
+VALID_SOURCES = {'env', 'stack_param'}
 
-ENV_KEY_FORMAT = '_CONFIG_{key}'
 TEMPLATE_FILE = '{root}/config/web_template.json'
 OUTPUT_FILE = '{root}/website/js/config/config.js'
 
 class GenerateConfig():
-    def setup(self):
+    def __init__(self):
+        self.client = boto3.client('cloudformation', region_name='us-east-1')
+
         self.root = os.environ.get('ROOTDIR', os.getcwd())
+        self.stack_name = os.environ['STACKNAME']
+
         self.template_file = TEMPLATE_FILE.format(root=self.root)
         self.output_filename = OUTPUT_FILE.format(root=self.root)
 
-    def run(self):
-        self.setup()
+        self.stack_exports = None
+        self.source_map = {
+            'env': self.get_from_env,
+            'stack_param': self.get_from_stack_export,
+        }
 
+    def get_stack_output_dict(self):
+        """Get stack outputs and convert to dict."""
+        stacks = self.client.describe_stacks(StackName=self.stack_name)
+        return {
+            output['OutputKey']: output['OutputValue']
+            for output in stacks['Stacks'][0]['Outputs']
+        }
+
+    def get_from_stack_export(self, key):
+        """Use a Cloudformation stack export as a config value."""
+        if self.stack_exports is None:
+            self.stack_exports = self.get_stack_output_dict()
+
+        return self.stack_exports[key]
+
+    def get_from_env(self, key):
+        """Use an environment variable as a config value."""
+        return os.environ[key]
+
+    def run(self):
         with open(self.template_file, 'r') as template_file:
             template_data = json.load(template_file)
 
-        output_data = {
-            key: os.environ[key] for key in template_data
-        }
+        for item in template_data.values():
+            if item['source'] not in VALID_SOURCES:
+                raise ValueError("Invalid config source: {}".format(value))
+
+        output_data = {}
+        for key, item in template_data.items():
+            source = item['source']
+            param_key = item.get('key', key)
+            output_data[key] = self.source_map[source](param_key)
 
         js_content = "const CONFIG = {config_json};".format(
             config_json=json.dumps(output_data, indent=4)
