530bbbd431018edc2da2836b21da7f85f3d09fbb
Andrew Seaman <andrewseaman35@gmail.com>
2018-11-20 17:08:44 -0800

    Enable access to stack params in JS config (#16)
    * Add cloudformation outputs to js config
    
    * enable key override since stack outputs must be alphanumeric
    
    * remove boto3 profile specification

--- website/js/index.js
+++ website/js/index.js
@@ -1,1 +1,10 @@
-console.log(CONFIG);
+const API_HOSTNAME = `https://${CONFIG.API_ID}.execute-api.us-east-1.amazonaws.com/test`;
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+    }
+};
+xhttp.open('GET', API_HOSTNAME, true);
+xhttp.send();
