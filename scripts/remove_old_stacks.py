9691e4cfcfbe039e52bc773d4e1739bd2887aff5
Andrew Seaman <andrewseaman35@gmail.com>
2018-12-07 15:06:30 -0800

    Bunch of backend setup in place, updating bucket names (#19)
    * add stack-create-complete (#11)
    
    * Create stack for feature branches (#12)
    
    * Deploy based on env and branch
    
    * deploy feature branches to branch path (#13)
    
    * basic testing hub for test environment (#14)
    
    * Add dynamic js config (#15)
    
    * Build and add JS config
    
    * Remove source for venv
    
    * Remove source for venv
    
    * fix exports in deploy_api
    
    * make config dir
    
    * fix test site, update config location
    
    * Enable access to stack params in JS config (#16)
    
    * Add cloudformation outputs to js config
    
    * enable key override since stack outputs must be alphanumeric
    
    * remove boto3 profile specification
    
    * remove ajax request
    
    * Delete old stacks after deploy of new stack (#17)
    
    * remove old stacks after deploy
    
    * fix deploy.sh name in .travis, add prints to remove script
    
    * fix print
    
    * try to delete stacks that failed to delete
    
    * Update bucket names (#18)

---
+++ scripts/remove_old_stacks.py
@@ -0,0 +1,67 @@
+import boto3
+import os
+import re
+
+REGEX_WITH_BRANCH = r'^stack-{env}-(?P<branch>.*)-(?P<nonce>[^-]*)$'
+REGEX_WITHOUT_BRANCH = r'^stack-{env}-(?P<nonce>[^-]*)$'
+
+STATES_TO_DELETE = ['CREATE_COMPLETE', 'DELETE_FAILED']
+VALID_ENVS = {'test', 'stage', 'live'}
+STACKNAME_REGEXES = {
+    'test': re.compile(REGEX_WITH_BRANCH.format(env='test')),
+    'stage': re.compile(REGEX_WITHOUT_BRANCH.format(env='stage')),
+    'live': re.compile(REGEX_WITHOUT_BRANCH.format(env='live')),
+}
+
+
+class RemoveOldStacks():
+    """
+    Deletes old stacks.
+
+    Requires environment variables:
+        DEPLOY_ENV: current deploy environment ('test', 'stage', 'live')
+        STACKNAME: name of currently deployed stack in environment (is not deleted)
+        BRANCH: if DEPLOY_ENV == 'test', branch to delete stacks for
+    """
+    def __init__(self):
+        self.client = boto3.client('cloudformation', region_name='us-east-1')
+
+        self.stack_name = os.environ['STACKNAME']
+        self.deploy_env = os.environ['DEPLOY_ENV']
+        if self.deploy_env == 'test':
+            self.branch = os.environ['BRANCH']
+
+        if self.deploy_env not in VALID_ENVS:
+            raise ValueError("Invalid deploy env: {}".format(self.deploy_env))
+
+    def run(self):
+        out = "Deleting old stacks for {}".format(self.deploy_env)
+        if self.deploy_env == 'test':
+            out += ", branch: {}".format(self.branch)
+        print(out)
+
+        stack_summaries = self.client.list_stacks(
+            StackStatusFilter=STATES_TO_DELETE
+        )['StackSummaries']
+
+        count = 0
+        regex = STACKNAME_REGEXES[self.deploy_env]
+        for summary in stack_summaries:
+            stack_name = summary['StackName']
+            match = regex.match(stack_name)
+            if not match or stack_name == self.stack_name:
+                continue
+            if self.deploy_env == 'test' and self.branch != match.group('branch'):
+                continue
+
+            count += 1
+            print("Deleting stack {}".format(stack_name))
+            response = self.client.delete_stack(
+                StackName=stack_name,
+            )
+
+        print("Deleted {} stacks".format(count))
+
+
+if __name__ == '__main__':
+    RemoveOldStacks().run()
