a4725bf5f88ad999ee2048599616647e2e62c630
Andrew Seaman <aseaman@23andme.com>
2019-01-08 16:48:04 -0800

    move some stuff around, only add aws profile if running locally

--- backend/lambdas/base/lambda_handler_base.py
+++ backend/lambdas/base/lambda_handler_base.py
@@ -7,28 +7,32 @@ import boto3
 
 
 class APILambdaHandlerBase(object):
-    def __init__(self):
+    def __init__(self, event, context):
+        self.event = event
+        self.context = context
+        self._parse_event(self.event)
         self._init_aws()
 
     def _init_aws(self):
-        raise NotImplementedError()
+        self.aws_session = (boto3.session.Session(profile_name='aseaman') if self.is_local
+                            else boto3.session.Session())
 
     def _parse_event(self, event):
         self.is_local = event.get('local', False)
 
-    def _before_run(self, event):
-        self._parse_event(event)
+    def _before_run(self):
+        pass
 
-    def _run(self, event, context):
+    def _run(self):
         raise NotImplementedError()
 
     def _after_run(self, result):
         print("result: {}".format(result))
 
-    def handle(self, event, context):
+    def handle(self):
         try:
-            self._before_run(event)
-            result = self._run(event, context)
+            self._before_run()
+            result = self._run()
             self._after_run(result)
         except Exception as e:
             print('Uh oh, error!')
--- backend/lambdas/recipes/lambda_handler.py
+++ backend/lambdas/recipes/lambda_handler.py
@@ -9,7 +9,7 @@ from base.lambda_handler_base import APILambdaHandlerBase
 
 class RecipeAPILambdaHandler(APILambdaHandlerBase):
     def _init_aws(self):
-        self.aws_session = boto3.session.Session(profile_name='aseaman')
+        super(RecipeAPILambdaHandler, self)._init_aws()
         self.s3_client = self.aws_session.client('s3', region_name='us-east-1')
 
     def _parse_event(self, event):
@@ -18,9 +18,9 @@ class RecipeAPILambdaHandler(APILambdaHandlerBase):
         if not self.recipe_url:
             raise ValueError('missing recipe_url in event')
 
-    def _run(self, event, context):
+    def _run(self):
         print(" -- Received event --")
-        print(json.dumps(event, indent=4))
+        print(json.dumps(self.event, indent=4))
 
         response = requests.get(self.recipe_url)
 
@@ -34,4 +34,4 @@ class RecipeAPILambdaHandler(APILambdaHandlerBase):
 
 
 def lambda_handler(event, context):
-    return RecipeAPILambdaHandler().handle(event, context)
+    return RecipeAPILambdaHandler(event, context).handle()
