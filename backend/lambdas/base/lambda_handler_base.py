4fea96f5345cc1394546a4958b75702585e36875
Andrew Seaman <andrewseaman35@gmail.com>
2019-01-09 16:27:42 -0800

    Clean up the lambda stuff, add POST to api gateway (#23)
    * Fix invocation of lambda functions locally, add test payload
    
    * move some stuff around, only add aws profile if running locally
    
    * Print event before parsing
    
    * Extract payload from event body
    
    * Update response body format
    
    * Fix response format
    
    * Return result from handle method
    
    * Add post to api-stack template
    
    * remove RequestTemplates from post
    
    * Update integration type for post

--- backend/lambdas/base/lambda_handler_base.py
+++ backend/lambdas/base/lambda_handler_base.py
@@ -6,28 +6,59 @@ import traceback
 import boto3
 
 
-class APILambdaHandlerBase():
+class APILambdaHandlerBase(object):
+    def __init__(self, event, context):
+        self.event = event
+        self.context = context
+        self._parse_event(self.event)
+        self._init_aws()
+
+    def _init_aws(self):
+        self.aws_session = (boto3.session.Session(profile_name='aseaman') if self.is_local
+                            else boto3.session.Session())
+
+    def _empty_response(self):
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 200,
+            "headers": {},
+            "body": json.dumps({})
+        }
+
+    def _parse_payload(self, payload):
+        raise NotImplementedError
+
     def _parse_event(self, event):
+        print(" -- Received event --")
+        print(json.dumps(event, indent=4))
+        print(" --                --")
+
         self.is_local = event.get('local', False)
 
-    def _before_run(self, event):
-        self._parse_event(event)
+        payload = event if self.is_local else json.loads(event['body'])
+        self._parse_payload(payload)
 
-    def _run(self, event, context):
+    def _before_run(self):
+        pass
+
+    def _run(self):
         raise NotImplementedError()
 
     def _after_run(self, result):
         print("result: {}".format(result))
 
-    def handle(self, event, context):
+    def handle(self):
+        result = self._empty_response()
         try:
-            self._before_run(event)
-            result = self._run(event, context)
+            self._before_run()
+            result = self._run()
             self._after_run(result)
         except Exception as e:
             print('Uh oh, error!')
             self._handle_error(e)
             traceback.print_exc()
+        finally:
+            return result
 
     def _handle_error(self, e):
         print("error: {}".format(e))
--- backend/lambdas/recipes/lambda_handler.py
+++ backend/lambdas/recipes/lambda_handler.py
@@ -8,18 +8,30 @@ from base.lambda_handler_base import APILambdaHandlerBase
 
 
 class RecipeAPILambdaHandler(APILambdaHandlerBase):
+    def _init_aws(self):
+        super(RecipeAPILambdaHandler, self)._init_aws()
+        self.s3_client = self.aws_session.client('s3', region_name='us-east-1')
 
-    def _run(self, event, context):
-        print(" -- Received event --")
-        print(json.dumps(event, indent=4))
+    def _parse_payload(self, payload):
+        self.recipe_url = payload.get('recipe_url')
+        if not self.recipe_url:
+            raise ValueError('missing recipe_url in event')
+
+    def _run(self):
+        response = requests.get(self.recipe_url)
+
+        response_body = {
+            "received": "Recipes - {}".format(self.recipe_url),
+            "response_length": len(response.content)
+        }
         return {
             "isBase64Encoded": False,
             "statusCode": 200,
             "headers": {},
             "multiValueHeaders": {},
-            "body": "Recipes"
+            "body": json.dumps(response_body)
         }
 
 
 def lambda_handler(event, context):
-    return RecipeAPILambdaHandler().handle(event, context)
+    return RecipeAPILambdaHandler(event, context).handle()
