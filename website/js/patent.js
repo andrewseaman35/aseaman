ef26077e97a79b725145858973633d373da01f8e
Andrew Seaman <aseaman@23andme.com>
2019-01-16 14:32:00 -0800

    It works

--- website/js/patent.js
+++ website/js/patent.js
@@ -1,10 +1,38 @@
 const data = {
     state_id: 'patent_number'
 };
+
+const url = `https://${CONFIG.API_ID}.execute-api.us-east-1.amazonaws.com/test/state_check`
+
+const newRow = function(label, value) {
+    const row = document.createElement('tr');
+    const labelCell = document.createElement('td');
+    labelCell.innerHTML = label;
+    const valueCell = document.createElement('td');
+    valueCell.innerHTML = value;
+
+    row.appendChild(labelCell);
+    row.appendChild(valueCell);
+
+    return row;
+}
+
 $.post(
-    "https://19bv595mn1.execute-api.us-east-1.amazonaws.com/test/state_check",
+    url,
     JSON.stringify(data),
     function(response) {
-        console.log(response)
+        document.getElementById('patent-state-loading').style.display = "none";
+        const table = document.getElementById('state-table');
+        const patentExists = response.data.available ? 'YES!' : 'no :(';
+        const status = response.data.app_status;
+        const lastUpdated = response.data.last_updated;
+        const patentNumber = response.data.available ? response.data.patent_number : 'not yet';
+
+        table.appendChild(newRow('Patent exists?', patentExists));
+        if (response.data.available) {
+            table.appendChild(newRow('Patent number', patentNumber));
+        }
+        table.appendChild(newRow('Latest status', status));
+        table.appendChild(newRow('Last updated', lastUpdated));
     },
 );
