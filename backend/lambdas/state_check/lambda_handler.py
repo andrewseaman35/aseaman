d8e77a443ac47392e5f983ef98c112f1a2738b57
Andrew Seaman <andrewseaman35@gmail.com>
2019-01-16 15:53:02 -0800

    Set up a few pages, get state check api working with patent number (#28)
    * add stack-create-complete (#11)
    
    * Create stack for feature branches (#12)
    
    * Deploy based on env and branch
    
    * deploy feature branches to branch path (#13)
    
    * basic testing hub for test environment (#14)
    
    * Add dynamic js config (#15)
    
    * Build and add JS config
    
    * Remove source for venv
    
    * Remove source for venv
    
    * fix exports in deploy_api
    
    * make config dir
    
    * fix test site, update config location
    
    * Enable access to stack params in JS config (#16)
    
    * Add cloudformation outputs to js config
    
    * enable key override since stack outputs must be alphanumeric
    
    * remove boto3 profile specification
    
    * remove ajax request
    
    * Delete old stacks after deploy of new stack (#17)
    
    * remove old stacks after deploy
    
    * fix deploy.sh name in .travis, add prints to remove script
    
    * fix print
    
    * try to delete stacks that failed to delete
    
    * Update bucket names (#18)
    
    * Empty s3 before uploading (#20)
    
    * Empty S3 buckets before deploying new site
    
    * add notes folder for non-committed stuff
    
    * Recipes api start (#21)
    
    * use base lambda handler class
    
    * add venv to dependency
    
    * fix cp
    
    * remove aws profile from cp
    
    * cleanup
    
    * remove unnecessary files
    
    * Add invoke.py to test lambda functions (#22)
    
    * Clean up the lambda stuff, add POST to api gateway (#23)
    
    * Fix invocation of lambda functions locally, add test payload
    
    * move some stuff around, only add aws profile if running locally
    
    * Print event before parsing
    
    * Extract payload from event body
    
    * Update response body format
    
    * Fix response format
    
    * Return result from handle method
    
    * Add post to api-stack template
    
    * remove RequestTemplates from post
    
    * Update integration type for post
    
    * organize page a little bit (#24)
    
    * Add homepage and about page (#25)
    
    * landing page content?
    
    * Add about page
    
    * State check initial function (#26)
    
    * Add state_check lambda function
    
    * Update access of ddb items
    
    * Update iam role
    
    * Update role policy
    
    * Update stack role
    
    * trying again..
    
    * separate common values from other values
    
    * fix stuff
    
    * Patent number page (#27)
    
    * Patent page and js, update headers in lambda response
    
    * It works
    
    * Add some stuff to the page

---
+++ backend/lambdas/state_check/lambda_handler.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+import boto3
+import requests
+
+from base.lambda_handler_base import APILambdaHandlerBase
+
+TABLE_NAME = 'states'
+
+
+class StateCheckAPILambdaHandler(APILambdaHandlerBase):
+    def _init_aws(self):
+        super(StateCheckAPILambdaHandler, self)._init_aws()
+        self.ddb_client = self.aws_session.client('dynamodb', region_name='us-east-1')
+
+    def _parse_payload(self, payload):
+        self.state_id = payload.get('state_id')
+        if not self.state_id:
+            raise ValueError('missing state_id in event')
+
+    def _get_state_data(self):
+        common_keys = {'id', 'time_updated'}
+        ddb_item = self.ddb_client.get_item(
+            TableName=TABLE_NAME,
+            Key={
+                'id': {
+                    'S': self.state_id,
+                }
+            }
+        )['Item']
+
+        result = {
+            key: ddb_item.pop(key)['S'] for key in common_keys
+        }
+        data = {
+            key: value[list(value.keys())[0]] for key, value in ddb_item.items()
+        }
+        result['data'] = data
+        return result
+
+    def _run(self):
+        item = self._get_state_data()
+
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "multiValueHeaders": {},
+            "body": json.dumps(item)
+        }
+
+
+def lambda_handler(event, context):
+    return StateCheckAPILambdaHandler(event, context).handle()
