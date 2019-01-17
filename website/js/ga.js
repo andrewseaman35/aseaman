6ef8d06b1e97120946f98c43eab35b857fb2c06f
Andrew Seaman <aseaman@23andme.com>
2019-01-17 10:04:56 -0800

    Update variable names

--- website/js/ga.js
+++ website/js/ga.js
@@ -9,8 +9,8 @@ function loadScript(url, callback) {
     head.appendChild(script);
 }
 
-const url = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.GA_TRACKING_ID}`;
-loadScript(url, function() {
+const googleTagManagerUrl = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.GA_TRACKING_ID}`;
+loadScript(googleTagManagerUrl, function() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
