b04236df3dd2ab3aed15c176d85d7cdb89fb6ff5
Andrew Seaman <aseaman@23andme.com>
2019-01-17 10:04:56 -0800

    Update variable names

--- website/js/index.js
+++ website/js/index.js
@@ -1,10 +1,1 @@
-const API_HOSTNAME = `https://${CONFIG.API_ID}.execute-api.us-east-1.amazonaws.com/test`;
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-    }
-};
-xhttp.open('GET', API_HOSTNAME, true);
-xhttp.send();
+console.log(CONFIG);
