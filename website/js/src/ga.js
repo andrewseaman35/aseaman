bed31b93c7b5a4b3d99b6d84a904231748715226
Andrew Seaman <andrewseaman35@gmail.com>
2019-02-04 11:49:17 -0800

    GA tracking, templating system, use npm (#40)
    * Enable access to stack params in JS config (#16)
    
    * Add cloudformation outputs to js config
    
    * enable key override since stack outputs must be alphanumeric
    
    * remove boto3 profile specification
    
    * Basic GA tracking (#29)
    
    * Add GA tracking id to parameter store and config
    
    * remove profile, add tracking stuff
    
    * suppress output
    
    * Add tracking to other pages
    
    * Add tracking to testing hub index
    
    * Update variable names
    
    * Templating system (#37)
    
    * Templating system for html
    
    * Add website/*.html to .gitignore
    
    * Add jquery back
    
    * Use NPM for js libraries (#39)

---
+++ website/js/src/ga.js
@@ -0,0 +1,24 @@
+const CONFIG = require('./config');
+
+function loadScript(url, callback) {
+    var head = document.head;
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    head.appendChild(script);
+}
+
+const loadGAScript = function(CONFIG) {
+    const googleTagManagerUrl = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.GA_TRACKING_ID}`;
+    loadScript(googleTagManagerUrl, function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', CONFIG.GA_TRACKING_ID);
+    });
+};
+
+module.exports = loadGAScript;
