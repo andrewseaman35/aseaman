3e05798046f9bf2509346bf1b781b124866e8f4c
Andrew Seaman <andrewseaman35@gmail.com>
2019-04-22 14:55:51 -0700

    Generate nav via JS, add nested nav (#54)

--- website/js/src/header.js
+++ website/js/src/header.js
@@ -5,6 +5,14 @@ const AUTH = require('./auth');
 const CONSTANTS = require('./constants');
 const CONFIG = require('./config');
 
+const NAV_STRUCTURE = [
+    { id: 'index', label: 'Home', href: 'index.html', items: [] },
+    { id: 'about', label: 'About', href: 'about.html', items: [
+        { id: 'pipeline', label: 'pipeline', href: 'pipeline.html' },
+    ]},
+    { id: 'patent', label: 'Patent', href: 'patent.html', items: [] },
+];
+
 const loginUrl = function(redirectURI) {
     return `${CONSTANTS.LOGIN_DOMAIN}login?response_type=token&client_id=${CONFIG.CLIENT_ID}&redirect_uri=${redirectURI}`;
 };
@@ -13,26 +21,43 @@ const logoutUrl = function(logoutURI) {
     return `${CONSTANTS.LOGIN_DOMAIN}logout?client_id=${CONFIG.CLIENT_ID}&logout_uri=${logoutURI}`;
 };
 
-const initLogin = function() {
+const navElement = function(className, item) {
+    return $(`<div class="${className}" id="nav-${item.id}"><a href="${item.href}">${item.label}</a></div>`);
+};
+
+const navItem = function(item) {
+    return navElement('nav-item', item);
+};
+
+const nestedNavItem = function(item) {
+    return navElement('nested-nav-item', item);
+};
+
+const appendLoginItem = function($nav) {
     const loggedIn = AUTH.getToken() !== null;
-    const linkElements = document.getElementsByClassName('log-in-link');
     const redirectURI = `${CONFIG.ROOT_URL}auth_callback.html`;
     const logoutURI = `${CONFIG.ROOT_URL}logout.html`;
 
     const linkUrl = loggedIn ? logoutUrl(logoutURI) : loginUrl(redirectURI);
-    for (let i = 0; i < linkElements.length; i++) {
-        linkElements[i].href = linkUrl;
-        linkElements[i].text = loggedIn ? 'Log out' : 'Log In';
-    }
+    const text = loggedIn ? 'Log out' : 'Log In';
+    navItem({id: 'login', href: linkUrl, label: text}).appendTo($nav);
 };
 
 const header = function() {
-    initLogin();
-
-    const navId = '#nav-' + document.body.id;
-    const navItem = document.querySelector(navId);
-    if (navItem) navItem.parentNode.removeChild(navItem);
-    document.querySelector('.nav-bar').style.visibility = 'visible';
+    const $navRow = $('.nav-bar');
+    NAV_STRUCTURE.forEach(function(item) {
+        const $navItem = navItem(item);
+        if (item.items.length > 0) {
+            const $nestedNav = $('<div class="nested-nav"></div>');
+            item.items.forEach(function(nestedItem) {
+                nestedNavItem(nestedItem).appendTo($nestedNav);
+            });
+            $nestedNav.appendTo($navItem);
+        }
+        $navItem.appendTo($navRow);
+    });
+    appendLoginItem($navRow);
+    $('.nav-bar').css('visibility', 'visible');
 };
 
 module.exports = header;
