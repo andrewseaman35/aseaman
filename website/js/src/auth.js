ae611a8a9934028cc55debc58c4cd07005b7ad23
Andrew Seaman <andrewseaman35@gmail.com>
2019-04-22 11:44:25 -0700

    Develop (#53)
    * Login and logout through Cognito hosted UI (#51)
    
    * Add login to cognito, but it doesn't do anything yet
    
    * Get stuff working again
    
    * Add initial pipeline page (#52)

---
+++ website/js/src/auth.js
@@ -0,0 +1,47 @@
+const utils = require('./utils');
+const CONST = require('./constants');
+
+const setToken = function(token) {
+    utils.setCookie('id_token', token, CONST.TOKEN_EXPIRATION_TIME);
+};
+
+const getToken = function() {
+    return utils.getCookie('id_token');
+};
+
+const unsetToken = function() {
+    utils.unsetCookie('id_token');
+};
+
+const extractAuthComponents = function(authUrl) {
+    const splitPath = authUrl.split('#');
+    if (splitPath.length !== 2) {
+        return null;
+    }
+    const queryString = splitPath[1];
+    const query = {};
+    const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (let i = 0; i < pairs.length; i++) {
+        const pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+
+};
+
+const authenticate = function() {
+    const url = window.location.href;
+    const components = extractAuthComponents(url);
+    if (!components) {
+        return false;
+    }
+    setToken(components.id_token);
+    window.location.replace('/');
+};
+
+const logout = function() {
+    unsetToken();
+    window.location.replace('/');
+};
+
+module.exports = { authenticate, setToken, getToken, logout };
---
+++ website/js/src/constants.js
@@ -0,0 +1,6 @@
+const CONSTANTS = {
+    LOGIN_DOMAIN: 'https://andrewcseaman.auth.us-east-1.amazoncognito.com/',
+    TOKEN_EXPIRATION_TIME: 3600,  // seconds
+};
+
+module.exports = CONSTANTS;
