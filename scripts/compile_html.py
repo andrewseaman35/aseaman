cb31d0a9ba6d966fa41dcfec341fe49ae41b0799
Andrew Seaman <andrewseaman35@gmail.com>
2019-04-22 11:44:25 -0700

    Develop (#53)
    * Login and logout through Cognito hosted UI (#51)
    
    * Add login to cognito, but it doesn't do anything yet
    
    * Get stuff working again
    
    * Add initial pipeline page (#52)

--- scripts/compile_html.py
+++ scripts/compile_html.py
@@ -9,7 +9,7 @@ TEMPLATE_DIRNAME = 'templates'
 WEBSITE_DIRNAME = 'website'
 PUBLIC_DIRNAME = 'public'
 
-PAGES = ['about', 'index', 'patent']
+PAGES = ['about', 'index', 'patent', 'auth_callback', 'logout' ,'pipeline']
 
 
 class CompileHTML(BaseScript):
