813975fa780ee180407d89b6b837c9e66c6ad1e1
Andrew Seaman <andrewseaman35@gmail.com>
2019-07-10 14:33:07 -0700

    Enable authing in lambda api base (#56)
    * Change statecheck lambda to authed
    
    * Fix stuff
    
    * update lambda policy
    
    * Move api key validation
    
    * Move auth to base
    
    * Disable auth for state check lambda

--- backend/lambdas/base/lambda_handler_base.py
+++ backend/lambdas/base/lambda_handler_base.py
@@ -6,16 +6,34 @@ import traceback
 import boto3
 
 
+SSM_API_KEY = 'lambda-api-key'
+
 class APILambdaHandlerBase(object):
+    REQUIRE_AUTH = True
+
     def __init__(self, event, context):
         self.event = event
         self.context = context
-        self._parse_event(self.event)
+
+        self.is_local = event.get('local', False)
+
+        self.__init_aws()
+        self.__parse_event(self.event)
+
+    def __init_aws(self):
+        self.aws_session = (boto3.session.Session(profile_name='aseaman') if self.is_local
+                            else boto3.session.Session())
+        self.ssm_client = self.aws_session.client('ssm', region_name='us-east-1')
         self._init_aws()
 
     def _init_aws(self):
-        self.aws_session = (boto3.session.Session(profile_name='aseaman') if self.is_local
-                            else boto3.session.Session())
+        pass
+
+    @property
+    def __api_key(self):
+        response = self.ssm_client.get_parameter(Name=SSM_API_KEY)
+        value = response['Parameter']['Value']
+        return value
 
     def _empty_response(self):
         return {
@@ -25,18 +43,26 @@ class APILambdaHandlerBase(object):
             "body": json.dumps({})
         }
 
+    def __parse_payload(self, payload):
+        if self.REQUIRE_AUTH:
+            self.api_key = payload.get('api_key')
+            if not self.api_key:
+                raise ValueError('missing api_key in event')
+            if not self.__api_key or not self.api_key == self.__api_key:
+                raise ValueError('invalid api key')
+
+        self._parse_payload(payload)
+
     def _parse_payload(self, payload):
         raise NotImplementedError
 
-    def _parse_event(self, event):
+    def __parse_event(self, event):
         print(" -- Received event --")
         print(json.dumps(event, indent=4))
         print(" --                --")
 
-        self.is_local = event.get('local', False)
-
         payload = event if self.is_local else json.loads(event['body'])
-        self._parse_payload(payload)
+        self.__parse_payload(payload)
 
     def _before_run(self):
         pass
