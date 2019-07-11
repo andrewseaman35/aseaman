de7bc396cc70961c6de9e7b647c87e8afa07f91c
Andrew Seaman <andrewseaman35@gmail.com>
2019-07-11 10:06:21 -0700

    Whisky shelf initial lambda function (#58)
    * Add lambda
    
    * Deploy api
    
    * actually add changes
    
    * Make new api resource
    
    * Fix resource name
    
    * update pathpart
    
    * fix invocations
    
    * Update role policy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,17 +8,20 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
     export DEPLOY_ENV="stage"
     export \
         STACKNAME=stack-$DEPLOY_ENV-$NONCE \
-        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$NONCE
+        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$NONCE \
+        WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$NONCE
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
     export DEPLOY_ENV="live"
     export \
         STACKNAME=stack-$DEPLOY_ENV-$NONCE \
-        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$NONCE
+        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$NONCE \
+        WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$NONCE
 else
     export DEPLOY_ENV="test"
     export \
         STACKNAME=stack-$DEPLOY_ENV-$BRANCH-$NONCE \
-        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$BRANCH-$NONCE
+        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$BRANCH-$NONCE \
+        WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$BRANCH-$NONCE
 fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
