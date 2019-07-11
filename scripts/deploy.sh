feeab513ad176e7f23bb842db80be973f3283334
Andrew Seaman <andrewseaman35@gmail.com>
2019-07-11 15:40:25 -0700

    Add some names to api stack (#60)
    * Add some names to api stack
    
    * update deploy scripts/makefiles

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,19 +8,19 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
     export DEPLOY_ENV="stage"
     export \
         STACKNAME=stack-$DEPLOY_ENV-$NONCE \
-        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$NONCE \
+        STATE_API_NAME=state-api-$DEPLOY_ENV-$NONCE \
         WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$NONCE
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
     export DEPLOY_ENV="live"
     export \
         STACKNAME=stack-$DEPLOY_ENV-$NONCE \
-        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$NONCE \
+        STATE_API_NAME=state-api-$DEPLOY_ENV-$NONCE \
         WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$NONCE
 else
     export DEPLOY_ENV="test"
     export \
         STACKNAME=stack-$DEPLOY_ENV-$BRANCH-$NONCE \
-        LAMBDA_FUNCTION_NAME=lambda-api-$DEPLOY_ENV-$BRANCH-$NONCE \
+        STATE_API_NAME=state-api-$DEPLOY_ENV-$BRANCH-$NONCE \
         WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$BRANCH-$NONCE
 fi
 
