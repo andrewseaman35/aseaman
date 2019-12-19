3096f2112908215c5329da9483c144b35c6e8f0f
Andrew Seaman <andrewseaman35@gmail.com>
2019-12-19 10:19:31 -0800

    Add react (#70)

---
+++ website/js/src/about.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div class="inner">
+                <div class="left-content">
+                    <h3>What is this?</h3>
+                    <p>
+                        This website is an ongoing project on mine that I'm using as a way to mess around and learn new things. It doesn't really
+                        have any goals except for being a mechanism for me to mess with new technologies, put things that I've done, and (hopefully)
+                        create useful tools.
+                    </p>
+                    <p>
+                        We'll see how many of those things I get to.
+                    </p>
+                    <a href="index.html">Go back home</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+function initAbout() {
+    ReactDOM.render(
+        <About />,
+        document.getElementById('about-container'),
+    );
+}
+
+module.exports = initAbout;
