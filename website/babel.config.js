8f48534219426d5f62051bdffa8a945513bb57a2
Andrew Seaman <andrewseaman35@gmail.com>
2019-12-19 10:19:31 -0800

    Add react (#70)

--- website/babel.config.js
+++ website/babel.config.js
@@ -2,14 +2,16 @@ const presets = [
     [
         '@babel/env',
         {
-            targets: {
-                edge: '17',
-                firefox: '60',
-                chrome: '67',
-                safari: '11.1',
+            corejs: {
+                version: 3,
+                proposals: true,
             },
+            targets: '> 0.25%, not dead',
             useBuiltIns: 'usage',
         },
+    ],
+    [
+        '@babel/preset-react',
     ],
 ];
 
