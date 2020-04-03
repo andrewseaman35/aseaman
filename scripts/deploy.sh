555188317aa5057b74db353014085ec607ce050e
Andrew Seaman <andrewseaman35@gmail.com>
2020-04-02 20:32:37 -0700

    Initial commit of draw jasper (#86)
    * Draw Jasper initial commit
    
    * fix yaml indentation
    
    * fix yaml
    
    * change import
    
    * More fixes
    
    * Require auth for now
    
    Co-authored-by: Andrew Seaman <aseaman@23andme.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,7 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
         API_URL=api-$NONCE.stage.andrewcseaman.com \
         STATE_API_NAME=state-api-$DEPLOY_ENV-$NONCE \
         WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$NONCE \
+        DRAW_JASPER_API_NAME=draw_jasper-api-$DEPLOY_ENV-$NONCE \
         HOSTED_ZONE_ID=Z1NTL75ESDHPUU \
         API_CERTIFICATE_ID=3aa91cb0-9bcb-4883-9fce-bb7a5d9da69d
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
@@ -20,6 +21,7 @@ elif [ "$TRAVIS_BRANCH" == "master" ]; then
         API_URL=api-$NONCE.live.andrewcseaman.com \
         STATE_API_NAME=state-api-$DEPLOY_ENV-$NONCE \
         WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$NONCE \
+        DRAW_JASPER_API_NAME=draw_jasper-api-$DEPLOY_ENV-$NONCE \
         HOSTED_ZONE_ID=Z1NTL75ESDHPUU \
         API_CERTIFICATE_ID=bc74e242-e6b1-4d85-9c7e-318c328340fb
 else
@@ -29,6 +31,7 @@ else
         API_URL=api-$NONCE.test.andrewcseaman.com \
         STATE_API_NAME=state-api-$DEPLOY_ENV-$BRANCH-$NONCE \
         WHISKY_API_NAME=whisky-api-$DEPLOY_ENV-$BRANCH-$NONCE \
+        DRAW_JASPER_API_NAME=draw_jasper-api-$DEPLOY_ENV-$BRANCH-$NONCE \
         HOSTED_ZONE_ID=Z1NTL75ESDHPUU \
         API_CERTIFICATE_ID=5af2f809-2459-4293-ae2d-d611ae4045bd
 fi
---
+++ website/js/src/DrawJasper.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import $ from 'jquery';
+
+import { getAPIUrl, getImageSrc } from './utils';
+
+
+class DrawJasper extends React.Component {
+    constructor() {
+        super();
+
+        this.renderMatch = this.renderMatch.bind(this);
+        this.clearCanvas = this.clearCanvas.bind(this);
+        this.saveCanvas = this.saveCanvas.bind(this);
+        this.uploadImage = this.uploadImage.bind(this);
+
+        this.state = {
+            drawingInProgress: false,
+            finishedDrawing: false,
+            imgSrc: null,
+            rankings: null,
+        };
+    }
+
+    componentDidMount() {
+        this.canvas = document.getElementById('jas-canvas');
+        this.ctx = this.canvas.getContext("2d");
+        this.ctx.fillStyle = "#FFFFFF";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.fillStyle = "#000000";
+        this.canvasTop = this.canvas.getBoundingClientRect().y;
+        this.canvasLeft = this.canvas.getBoundingClientRect().x;
+        this.dragStart = null;
+        this.previousPos = null;
+
+        this.initializeEventListeners();
+    }
+
+    initializeEventListeners() {
+        this.canvas.addEventListener("mousemove", (e) => {
+            this.handleMove(e)
+        }, false);
+        this.canvas.addEventListener("mousedown", (e) => {
+            this.handleDown(e)
+        }, false);
+        this.canvas.addEventListener("mouseup", (e) => {
+            this.handleUp(e)
+        }, false);
+    }
+
+    getMouseCoords(e) {
+        const x = e.clientX - this.canvasLeft;
+        const y = e.clientY - this.canvasTop;
+        return { x, y };
+    }
+
+    clearCanvas() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.setState({
+            drawingInProgress: false,
+            finishedDrawing: false,
+        });
+    }
+
+    saveCanvas() {
+        this.setState({
+            imgSrc: this.canvas.toDataURL("image/png"),
+        });
+    }
+
+    uploadImage(e) {
+        e.preventDefault();
+
+        $.ajax({
+          type: "POST",
+          url: getAPIUrl('draw_jasper'),
+          data:JSON.stringify({
+             img: this.state.imgSrc
+          }),
+          contentType: 'application/json',
+        }).promise().then((success) => {
+            this.setState({
+                rankings: success.results,
+            });
+        })
+    }
+
+    handleMove(e) {
+        if (this.state.finishedDrawing) {
+            return;
+        }
+        if (this.state.drawingInProgress) {
+            const { x, y} = this.getMouseCoords(e);
+            this.ctx.lineTo(x, y);
+            this.ctx.stroke();
+            this.previousPos = { x, y };
+        }
+    }
+
+    handleDown(e) {
+        if (this.state.finishedDrawing) {
+            return;
+        }
+        if (!this.state.drawingInProgress) {
+            this.setState({ drawingInProgress: true });
+            const { x, y } = this.getMouseCoords(e);
+            this.dragStart = { x, y };
+            this.previousPos = { x, y };
+            this.ctx.beginPath();
+            this.ctx.lineWidth = "5";
+            this.ctx.strokeStyle = "black"; // Green path
+            this.ctx.fillStyle = "black"; // Green path
+            this.ctx.moveTo(x, y);
+            this.ctx.lineTo(x, y);
+            this.ctx.stroke();
+        } else {
+            this.handleUp(e);
+        }
+    }
+
+    handleUp(e) {
+        if (this.state.finishedDrawing) {
+            return;
+        }
+        this.ctx.moveTo(this.previousPos.x, this.previousPos.y);
+        this.ctx.lineTo(this.dragStart.x, this.dragStart.y);
+        this.ctx.stroke();
+        this.ctx.fill()
+        this.dragStart = null;
+        this.setState({
+            drawingInProgress: false,
+            finishedDrawing: true,
+        })
+
+    }
+
+    renderMatch() {
+        console.log(this.state.rankings)
+        const matchId = this.state.rankings[0][1];
+        return (
+            <img width="400px" src={getImageSrc(`images/jasper/data/original/${matchId}.jpg`)} />
+        )
+    }
+
+    renderImageForm() {
+        return (
+            <form name="drawing" id="imageUploadForm" encType="multipart/form-data" method="post">
+                <input type="file" id="ImageBrowse" hidden="hidden" name="image" size="30"/>
+                <input onClick={this.uploadImage} type="submit" name="upload" value="Upload" />
+                <img id="drawing-image" src={this.state.imgSrc} />
+            </form>
+        )
+    }
+
+    renderCanvas() {
+        return (
+            <canvas
+                id="jas-canvas"
+                width="800"
+                height="400"
+            />
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                { this.state.rankings ? this.renderMatch() : null}
+                { this.state.imgSrc ? this.renderImageForm() : this.renderCanvas() }
+                <div className="action-bar">
+                    <button onClick={this.clearCanvas}>Clear</button>
+                    <button onClick={this.saveCanvas}>Save</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+module.exports = DrawJasper;
