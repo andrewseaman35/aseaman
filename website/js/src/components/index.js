05a5b2239f0d98d2d217b31519f1960ac55c58f7
Andrew Seaman <andrewseaman35@gmail.com>
2020-05-31 23:34:19 -0700

    Add lightbox

--- website/js/src/components/index.js
+++ website/js/src/components/index.js
@@ -1,12 +1,15 @@
 import { initWhiskyShelf } from './whisky/WhiskyShelf';
 
 import Icon from './icon/Icon';
+import Lightbox from './Lightbox';
 import Modal from './modal/Modal';
 import WhiskyShelf from './whisky/WhiskyShelf';
 import WhiskyForm from './whisky/WhiskyForm';
 
+
 module.exports = {
     Icon,
+    Lightbox,
     Modal,
     WhiskyForm,
     WhiskyShelf,
