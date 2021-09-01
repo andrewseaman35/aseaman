986a6b14da993ec46c00bc5e90c2008de52ad54a
Andrew Seaman <andrewseaman@andrews-air.lan>
2021-08-31 17:09:41 -0700

    add more terraform

---
+++ backend/modules/roles/mame_highscore/main.tf
@@ -0,0 +1,45 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  aws_region           = data.aws_region.current.name
+  aws_account_id       = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_iam_role_policy" "api_role" {
+  name = "${var.api_name}_role"
+  role = var.role
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:*",
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:ListBucket",
+          "s3:GetObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::aseaman-public-bucket",
+          "arn:aws:s3:::aseaman-public-bucket/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter"
+        ]
+        Resource = "arn:aws:ssm:${local.aws_region}:${local.aws_account_id}:parameter/*"
+      }
+    ]
+  })
+}
