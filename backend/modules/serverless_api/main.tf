db266d978a2968849b678182a5ce82aac29ebd1d
Andrew Seaman <andrewseaman35@gmail.com>
2022-02-18 13:12:40 -0800

    add proxy path part

--- backend/modules/serverless_api/main.tf
+++ backend/modules/serverless_api/main.tf
@@ -191,6 +191,125 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   ]
 }
 
+resource "aws_api_gateway_resource" "proxy_api_resource" {
+  parent_id   = aws_api_gateway_resource.api_resource.id
+  path_part   = "{proxy+}"
+  rest_api_id = var.rest_api_id
+}
+
+resource "aws_api_gateway_method" "proxy_any" {
+  authorization = "NONE"
+  http_method   = "ANY"
+  resource_id   = aws_api_gateway_resource.proxy_api_resource.id
+  rest_api_id   = var.rest_api_id
+}
+
+resource "aws_api_gateway_integration" "proxy_any_integration" {
+  rest_api_id             = var.rest_api_id
+  resource_id             = aws_api_gateway_resource.proxy_api_resource.id
+  http_method             = aws_api_gateway_method.proxy_any.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api_lambda_function.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "proxy_any_200" {
+  rest_api_id = var.rest_api_id
+  resource_id   = aws_api_gateway_resource.proxy_api_resource.id
+  http_method   = aws_api_gateway_method.proxy_any.http_method
+  status_code   = 200
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = true,
+      "method.response.header.Access-Control-Allow-Methods" = true,
+      "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  depends_on = [
+    aws_api_gateway_method.proxy_any
+  ]
+}
+
+resource "aws_api_gateway_integration_response" "proxy_any_integration_response" {
+  rest_api_id = var.rest_api_id
+  resource_id = aws_api_gateway_resource.proxy_api_resource.id
+  http_method = aws_api_gateway_method.proxy_any.http_method
+  status_code = "${aws_api_gateway_method_response.proxy_any_200.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.proxy_any_integration
+  ]
+}
+
+resource "aws_api_gateway_method" "proxy_options" {
+  rest_api_id   = var.rest_api_id
+  resource_id   = aws_api_gateway_resource.proxy_api_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "proxy_options_integration" {
+  rest_api_id             = var.rest_api_id
+  resource_id             = aws_api_gateway_resource.proxy_api_resource.id
+  http_method             = aws_api_gateway_method.proxy_options.http_method
+  type                    = "MOCK"
+
+  depends_on = [aws_api_gateway_method.proxy_options]
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "proxy_options_200" {
+  rest_api_id   = var.rest_api_id
+  resource_id   = aws_api_gateway_resource.proxy_api_resource.id
+  http_method   = aws_api_gateway_method.proxy_options.http_method
+  status_code   = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = true,
+      "method.response.header.Access-Control-Allow-Methods" = true,
+      "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.proxy_options_integration
+  ]
+}
+
+resource "aws_api_gateway_integration_response" "proxy_options_integration_response" {
+  rest_api_id = var.rest_api_id
+  resource_id = aws_api_gateway_resource.proxy_api_resource.id
+  http_method = aws_api_gateway_method.proxy_options.http_method
+  status_code = "${aws_api_gateway_method_response.proxy_options_200.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+
+  depends_on = [
+    aws_api_gateway_method_response.proxy_options_200
+  ]
+}
+
 resource "aws_lambda_permission" "api_invoke_function" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
