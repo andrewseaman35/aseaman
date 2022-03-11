0a403d6036516d542e5535a116de662b08589200
Andrew Seaman <andrewseaman35@gmail.com>
2022-03-10 18:34:17 -0800

    Add initial linker setup

---
+++ website/js/src/linker.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+import { getToken } from './auth';
+import { getAPIUrl } from './utils';
+
+function fetchLink(id) {
+    return $.ajax({
+        type: 'GET',
+        url: getAPIUrl('linker'),
+        headers: {
+            Authorization: getToken(),
+        },
+        data: {
+            'id': id,
+        },
+        contentType: 'application/json',
+    }).promise();
+}
+
+const link = function() {
+    const url = window.location.href;
+    const splitUrl = url.split('#');
+
+    if (splitUrl.length !== 2) {
+        window.location.replace('/index.html');
+    }
+
+    const linkId = splitUrl[1];
+    fetchLink(linkId).then((result) => {
+        window.location.replace(result.url);
+    }, () => {
+        window.location.replace('/404.html')
+    })
+};
+
+
+module.exports = {
+    link
+ };
