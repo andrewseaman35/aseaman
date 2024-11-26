e6ee2d41e810cfc2f00d079047dfa3c1e2aadf0d
Andrew Seaman <andrewseaman35@gmail.com>
2024-11-25 19:46:09 -0800

    add download all action to pull highscore file script

--- scripts/pull_highscore_file.py
+++ scripts/pull_highscore_file.py
@@ -22,32 +22,43 @@ class PullHighscoreFile(BaseScript):
         super(PullHighscoreFile, self)._setup_parser()
         self.parser.add_argument("--list", help="list games", action="store_true")
         self.parser.add_argument("--download", help="game id to download")
+        self.parser.add_argument(
+            "--download-all", help="downloads all files", action="store_true"
+        )
 
     def _validate_args(self):
         super(PullHighscoreFile, self)._validate_args()
         if self.args.download and self.args.list:
             raise Exception("one action only")
-        if not self.args.download and not self.args.list:
+        if not (self.args.download or self.args.list or self.args.download_all):
             raise Exception("one action, please")
 
-    def _list(self):
+    def _get_list(self):
         response = self.s3_client.list_objects_v2(
             Bucket=BUCKET_NAME,
             Prefix="hi/",
         )
-        keys = [
+        return [
             c["Key"].split("hi/")[1]
             for c in response["Contents"]
             if c["Key"].split("hi/")[1]
         ]
+
+    def _list(self):
+        keys = self._get_list()
         print("")
         print("== Available highscore files ==")
         for key in keys:
             print(key)
         print("")
 
-    def _download(self):
-        filename = self.args.download.split(".hi")[0] + ".hi"
+    def _download_all(self):
+        keys = self._get_list()
+        for key in keys:
+            self._download_file(key)
+
+    def _download_file(self, filename):
+        print(f"Downloading: {filename}")
         key = "hi/" + filename
         local_filename = os.path.join(DOWNLOAD_DIR, filename)
         with open(local_filename, "wb") as data:
@@ -62,10 +73,16 @@ class PullHighscoreFile(BaseScript):
         )
         print("")
 
+    def _download(self):
+        filename = self.args.download.split(".hi")[0] + ".hi"
+        self._download_file(filename)
+
     def _run(self):
         if self.args.list:
             self._list()
-        elif self.args.download:
+        elif self.args.download_all:
+            self._download_all()
+        else:
             self._download()
 
 
