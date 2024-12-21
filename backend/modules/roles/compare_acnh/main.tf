6c8c59d597e1faf4559cab8175bf821692095b41
Andrew Seaman <andrewseaman35@gmail.com>
2024-12-21 11:21:16 -0800

    update compare_acnh api role

--- backend/modules/roles/compare_acnh/main.tf
+++ backend/modules/roles/compare_acnh/main.tf
@@ -5,6 +5,8 @@ data "aws_caller_identity" "current" {}
 locals {
   aws_region     = data.aws_region.current.name
   aws_account_id = data.aws_caller_identity.current.account_id
+  results_table_name     = "${var.deploy_env == "live" ? "compare_acnh_results" : "compare_acnh_results_stage"}"
+  summary_table_name     = "${var.deploy_env == "live" ? "compare_acnh_summary" : "compare_acnh_summary_stage"}"
 }
 
 resource "aws_iam_role_policy" "api_role" {
@@ -32,8 +34,8 @@ resource "aws_iam_role_policy" "api_role" {
           "dynamodb:Query"
         ]
         Resource = [
-          "arn:aws:dynamodb:${local.aws_region}:${local.aws_account_id}:table/compare_acnh_results",
-          "arn:aws:dynamodb:${local.aws_region}:${local.aws_account_id}:table/compare_acnh_summary"
+          "arn:aws:dynamodb:${local.aws_region}:${local.aws_account_id}:table/${local.results_table_name}",
+          "arn:aws:dynamodb:${local.aws_region}:${local.aws_account_id}:table/${local.summary_table_name}"
         ]
       },
       {
