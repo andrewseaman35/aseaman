282de317a3caf85f3039ef0683a699ca19792d88
Andrew Seaman <andrewseaman35@gmail.com>
2024-12-20 23:06:27 -0800

    update salt level

--- backend/api/salt_level/lambda_handler.py
+++ backend/api/salt_level/lambda_handler.py
@@ -1,48 +1,48 @@
 import json
-import time
 
 from base.lambda_handler_base import APILambdaHandlerBase
+from base.dynamodb import DynamoDBItem, DynamoDBItemValueConfig, DynamoDBTable
 
-TABLE_NAME = "salt_level"
-LOCAL_TABLE_NAME = "salt_level_local"
+
+class SaltLevelDDBItem(DynamoDBItem):
+    _config = {
+        "water_softener_id": DynamoDBItemValueConfig(
+            "S",
+        ),
+        "timestamp": DynamoDBItemValueConfig("N"),
+        "sensor_0": DynamoDBItemValueConfig("N"),
+        "sensor_1": DynamoDBItemValueConfig("N"),
+        "sensor_2": DynamoDBItemValueConfig("N"),
+        "sensor_3": DynamoDBItemValueConfig("N"),
+    }
+
+
+class SaltLevelTable(DynamoDBTable):
+    ItemClass = SaltLevelDDBItem
 
 
 class SaltLevelLambdaHandler(APILambdaHandlerBase):
     primary_partition_key = "water_softener_id"
 
-    def _init(self):
-        self.table_name = LOCAL_TABLE_NAME if self.is_local else TABLE_NAME
-
-    def _init_aws(self):
-        self.ddb_client = self.aws_session.client("dynamodb", region_name="us-east-1")
+    aws_config = {
+        "dynamodb": {"enabled": True, "tables": [("salt_level", SaltLevelTable)]}
+    }
 
     def _scan_all(self):
-        return self.ddb_client.scan(
-            TableName=self.table_name,
-        )["Items"]
+        return self.aws["dynamodb"]["tables"]["salt_level"].scan()
 
     def _scan_by_water_softener_id(self, water_softener_id):
-        return self.ddb_client.scan(
-            TableName=self.table_name,
-            ExpressionAttributeNames={
-                "#wsid": "water_softener_id",
-            },
-            ExpressionAttributeValues={
-                ":wsid": {
-                    "S": water_softener_id,
-                },
-            },
-            FilterExpression="#wsid = :wsid",
-        )["Items"]
+        scan_dict = {"water_softener_id": water_softener_id}
+        return self.aws["dynamodb"]["tables"]["salt_level"].scan(scan_dict)
 
     def handle_get(self):
         water_softener_id = self.params.get("water_softener_id")
         if water_softener_id:
-            ddb_items = self._scan_by_water_softener_id(water_softener_id)
+            items = self._scan_by_water_softener_id(water_softener_id)
         else:
-            ddb_items = self._scan_all()
+            items = self._scan_all()
 
-        items = [self._ddb_item_to_json(ddb_item) for ddb_item in ddb_items]
+        items = [item.to_dict() for item in items]
         return {
             **self._empty_response(),
             "body": json.dumps(items),
