d7bffad06623528163446ac245dc756d7e97b148
Andrew Seaman <andrewseaman35@gmail.com>
2024-12-24 21:57:17 -0800

    add view selector

---
+++ website/js/src/budget/components/ViewSelector.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, } from 'react';
+import PropTypes from 'prop-types';
+
+const VIEWS = ['summary', 'upload']
+
+const ViewSelector = (props) => {
+
+    return (
+        <div className="view-selector">
+            {
+                VIEWS.map((view) => (
+                    <div
+                        className={`view-option ${view === props.selectedView ? 'selected' : ''}`}
+                        onClick={() => props.onViewChanged(view)}
+                    >{view}</div>
+                    )
+                )
+            }
+        </div>
+    )
+}
+
+ViewSelector.defaultProps = {
+    selectedView: null,
+};
+
+ViewSelector.propTypes = {
+    selectedView: PropTypes.string,
+    onViewChanged: PropTypes.func.isRequired,
+};
+
+export default ViewSelector;
--- website/js/src/budget/index.js
+++ website/js/src/budget/index.js
@@ -3,16 +3,28 @@ import $ from 'jquery';
 
 import FileUploader from '../components/FileUploader';
 import SummaryView from './views/SummaryView';
-
+import ViewSelector from './components/ViewSelector';
 
 const BudgetPage = (props) => {
     console.log("Rendering")
+    const [view, setView] = useState(null);
+
+    const renderView = (view) => {
+        if (view === "summary") {
+            return <SummaryView />
+        } else if (view === "upload") {
+            return <FileUploader />
+        }
+        return null;
+    }
 
     return (
-        <>
-            <SummaryView/>
-            <FileUploader />
-        </>
+        <div className="budget-content">
+            <ViewSelector
+                onViewChanged={setView}
+            />
+            {renderView(view)}
+        </div>
     )
 };
 
